0a9f5c76805cf9b170c3249dcb97bb3faaec0011
Vito <topvetal@gmail.com>
2021-01-07 22:44:26 +0200

    1st comm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
-
-function App() {
+let primary = 'no';
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Simple HTML
+      <ul>
+        <li className={`app${primary}`}>New item</li>
+        <li className={`app${primary}`}>New item2</li>
+        <li className={`app${primary}`}>New item3</li>
+        <li className={`app${primary}`}>New item4</li>
+      </ul>
     </div>
-  );
+  )
 }
-
+// const Welcome = () => <h1>Welcome to IT</h1>
 export default App;
+// export default Welcome;
