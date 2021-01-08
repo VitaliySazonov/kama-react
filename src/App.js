cea880f4704cec99b6ecf885c180fada6d5951c2
Vito <topvetal@gmail.com>
2021-01-08 14:13:35 +0200

    #1_social_1st_commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import logo from "./logo.svg";
 import React from "react";
 import "./App.css";
-import Header from "./Header";
-import Technologies from "./Technologies";
-let primary = "no";
+// import Header from "./Header"
+// import Technologies from "./Technologies"
+let isAdmin = false; // admin IP
+let ID = '176.168.2.1';
+let admClass = isAdmin ? `yes ${ID}` : 'no';
 const App = () => {
   return (
-    <section>
-      <img src={logo} alt="" />
-      <Header />
-      <Technologies />
+    <section
+      className={`app-wrapper`}
+    >
+      <header className={`header`}>
+        <img
+          src={logo}
+          alt=""
+        />
+      </header>
+      <nav
+        className={`nav`}
+      >
+        <div><a href="#Profile">Profile</a></div>
+        <div><a href="#Messages">Messages</a></div>
+        <div><a href="#News">News</a></div>
+        <div><a href="#Music">Music</a></div>
+        <div><a href="#Settings">Settings</a></div>
+      </nav>
+      <div
+        className={`content`}
+      >
+        Some content
+      </div>
     </section>
   );
 };
