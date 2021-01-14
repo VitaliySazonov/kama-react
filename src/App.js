04a69680ec89e097ab24a13c4532e150408d7e7b
Vito <topvetal@gmail.com>
2021-01-14 15:19:26 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import logo from "./logo.svg";
 import React from "react";
 import "./App.css";
-// import Header from "./Header"
-// import Technologies from "./Technologies"
-let isAdmin = false; // admin IP
-let ID = '176.168.2.1';
-let admClass = isAdmin ? `yes ${ID}` : 'no';
+import Header from './components/Header'
+import Navbar from "./components/Navbar"
+import Profile from "./components/Profile"
 const App = () => {
   return (
-    <section
-      className={`app-wrapper`}
-    >
-      <header className={`header`}>
-        <img
-          src={logo}
-          alt=""
-        />
-      </header>
-      <nav
-        className={`nav`}
-      >
-        <div><a href="#Profile">Profile</a></div>
-        <div><a href="#Messages">Messages</a></div>
-        <div><a href="#News">News</a></div>
-        <div><a href="#Music">Music</a></div>
-        <div><a href="#Settings">Settings</a></div>
-      </nav>
-      <div
-        className={`content`}
-      >
-        Some content
-      </div>
+    <section className={`app-wrapper`}>
+      <Header />
+      <Navbar />
+      <Profile/>
     </section>
   );
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import logo from "../logo.svg";
+
+const Header = () => {
+  return (
+    <header className={`header`}>
+      <img src={logo} alt="" />
+    </header>
+  );
+};
+
+export default Header;
